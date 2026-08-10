NVIDIA-NeMo/Gym

feat(token-id-capture): resolve each call's parent at request time
Identifies which recorded call a request continues, from what the harness already
sends: hashing the model-authored turns finds the call that produced the last one,
and the conversation those turns arrived with is verified before the match is used,
so a compacted or rewritten history does not resolve.

The record gains parent_call_id, cum_len and digest, and the builder prefers a
verified link over inferring one from token prefixes, which makes a chain exact
rather than inferred and tells a retry from the call the harness kept.

Signed-off-by: Ananth Subramaniam <ansubramania@nvidia.com>

tests/unit_tests/test_trajectory_builder.py
@@ -18,10 +18,12 @@ import pytest
 
 from nemo_gym.token_id_capture import (
     assert_prefix_contiguity,
+    compute_digest,
     per_request,
     prefix_merging,
     project_chain_to_output_items,
     project_main_chain_response,
+    stamp_lineage,
     token_id_capture_dirs_from_config,
     trajectories_for_rollout,
 )
@@ -29,8 +31,8 @@ from nemo_gym.token_id_capture.records import TokenEntry
 from nemo_gym.token_id_capture.store import TokenCaptureStore
 
 
-def _entry(mcid, prompt, gen, lp=None, created_at=0.0):
-    return TokenEntry(
+def _entry(mcid, prompt, gen, parent=None, lp=None, created_at=0.0):
+    e = TokenEntry(
         rollout_id="t0-r0",
         model_call_id=mcid,
         model="m",
@@ -40,6 +42,8 @@ def _entry(mcid, prompt, gen, lp=None, created_at=0.0):
         # Stamped when the call completed. Chain selection orders on it.
         created_at=created_at,
     )
+    stamp_lineage(e, parent)
+    return e
 
 
 # An append-only 3-call rollout: each call's prompt extends the prior prompt+generation
@@ -208,6 +212,19 @@ def test_projection_handles_content_only_leading_item():
     assert "prompt_token_ids" not in resp["output"][0]
     assert resp["usage"] == {"input_tokens": 3, "output_tokens": 2}  # counts from the token-bearing item
     assert_prefix_contiguity(resp)
+
+
+def test_retry_sibling_is_dropped_and_main_chain_is_deterministic():
+    # c2a and c2b are a retry pair (identical prompt, divergent generation). c3 extends c2a.
+    c1 = _entry("c1", [1, 2, 3], [10, 11])
+    c2a = _entry("c2a", [1, 2, 3, 10, 11, 4], [12])
+    c2b = _entry("c2b", [1, 2, 3, 10, 11, 4], [99])
+    c3 = _entry("c3", [1, 2, 3, 10, 11, 4, 12, 5], [13])
+    out = prefix_merging([c1, c2a, c2b, c3])
+    assert "c2b" in out.quarantined  # unextended retry sibling dropped
+    main = next(c for c in out.chains if c.chain_id == "main")
+    assert [link.entry.model_call_id for link in main.links] == ["c1", "c2a", "c3"]
+    assert_prefix_contiguity(project_main_chain_response("t0-r0", out))
 
 
 def test_consumer_reads_store_and_builds(tmp_path):
@@ -440,6 +457,111 @@ def test_the_builder_runs_once_per_rollout(tmp_path, monkeypatch):
 
     assert calls == ["prefix_merging"]
     assert built["metrics"]["n_calls"] == 2
+
+
+def _with_lineage(entry, parent_call_id=None):
+    stamp_lineage(entry, parent_call_id)
+    return entry
+
+
+def test_recorded_parent_link_resolves_a_final_call_retry_exactly():
+    """Two siblings share a prompt and differ only in their generation.
+
+    Prefix inference cannot tell which one the harness kept, because both are
+    equally valid children. A recorded parent link on the next call names the
+    survivor, so the other is provably unused rather than tie-broken.
+    """
+    root = _with_lineage(_entry("root", [1, 2], [3]))
+    kept = _with_lineage(_entry("kept", [1, 2, 3, 4], [5]), parent_call_id="root")
+    dropped = _with_lineage(_entry("dropped", [1, 2, 3, 4], [9]), parent_call_id="root")
+    # The next call continued `kept`, and says so.
+    nxt = _with_lineage(_entry("next", [1, 2, 3, 4, 5, 6], [7]), parent_call_id="kept")
+
+    out = prefix_merging([root, kept, dropped, nxt])
+    main = next(c for c in out.chains if c.chain_id == "main")
+    assert [link.entry.model_call_id for link in main.links] == ["root", "kept", "next"]
+    assert "dropped" in out.quarantined
+    # Resolved, so nothing is flagged for masking.
+    assert out.notes.unresolved_retries == []
+
+
+def test_unresolvable_final_retry_is_flagged_not_silently_tie_broken():
+    """A retry of the LAST call has no successor to name the survivor. Neither
+    inference nor a parent link can resolve it, so it must be reported so the
+    caller can mask the rollout instead of training on a generation the client
+    may never have received."""
+    root = _with_lineage(_entry("root", [1, 2], [3]))
+    a = _with_lineage(_entry("a", [1, 2, 3, 4], [5]), parent_call_id="root")
+    b = _with_lineage(_entry("b", [1, 2, 3, 4], [9]), parent_call_id="root")
+
+    out = prefix_merging([root, a, b])
+    assert sorted(out.notes.unresolved_retries) == ["a", "b"]
+
+
+def test_a_stale_parent_link_fails_verification_and_falls_back():
+    """A rerun that appended onto a previous attempt's records must not merge two
+    attempts. The digest check catches the bad edge; the builder falls back to
+    inference and reports that it did."""
+    root = _with_lineage(_entry("root", [1, 2], [3]))
+    child = _entry("child", [1, 2, 3, 4], [5])
+    stamp_lineage(child, "root")
+    # Corrupt the recorded parent's digest, as a stale record would.
+    root.digest = compute_digest([42, 42, 42])
+
+    out = prefix_merging([root, child])
+    assert out.notes.parent_link_fallbacks == {"parent_digest_mismatch": 1}
+    # Inference still finds the right parent, so the chain is intact.
+    main = next(c for c in out.chains if c.chain_id == "main")
+    assert [link.entry.model_call_id for link in main.links] == ["root", "child"]
+
+
+def test_parent_link_and_inference_agree_on_a_clean_rollout():
+    """Parity: with and without recorded links, the same rollout must stitch the
+    same way. This is what makes the lineage fields safe to add before anything
+    populates them."""
+    plain = [
+        _entry("c1", [1, 2, 3], [4, 5]),
+        _entry("c2", [1, 2, 3, 4, 5, 6], [7]),
+        _entry("c3", [1, 2, 3, 4, 5, 6, 7, 8], [9, 10]),
+    ]
+    linked = [
+        _with_lineage(_entry("c1", [1, 2, 3], [4, 5])),
+        _with_lineage(_entry("c2", [1, 2, 3, 4, 5, 6], [7]), parent_call_id="c1"),
+        _with_lineage(_entry("c3", [1, 2, 3, 4, 5, 6, 7, 8], [9, 10]), parent_call_id="c2"),
+    ]
+    inferred = prefix_merging(plain)
+    recorded = prefix_merging(linked)
+
+    def shape(out):
+        return [([link.entry.model_call_id for link in c.links], c.root_prompt) for c in out.chains]
+
+    assert shape(inferred) == shape(recorded)
+
+
+def test_a_recorded_parent_is_verified_not_trusted():
+    """A rerun appends to the same rollout file, so a record can name a parent from a previous
+    attempt whose tokens are an unrelated sequence. Chaining the two would train on a
+    conversation that never happened, so the digest check has to reject it and fall back."""
+    previous_attempt = _entry("old", [1, 2, 3], [4, 5])
+    this_attempt = _entry("new", [90, 91, 92, 93], [94], parent="old")
+
+    out = prefix_merging([previous_attempt, this_attempt])
+
+    assert "parent_digest_mismatch" in out.notes.parent_link_fallbacks
+    for chain in out.chains:
+        assert [link.entry.model_call_id for link in chain.links] != ["old", "new"]
+
+
+def test_a_correct_parent_link_is_used():
+    """The other half: a link that verifies must be honoured, or the recorded lineage buys
+    nothing over inferring the parent from token prefixes."""
+    parent = _entry("p", [1, 2], [3, 4])
+    child = _entry("c", [1, 2, 3, 4, 5], [6], parent="p")
+
+    out = prefix_merging([parent, child])
+
+    assert out.notes.parent_link_fallbacks == {}
+    assert any([link.entry.model_call_id for link in chain.links] == ["p", "c"] for chain in out.chains)
 
 
 def test_a_chain_that_breaks_is_split_and_reported():

swiss-ai/nemo-gym

tests/unit_tests/test_trajectory_builder.py
@@ -18,10 +18,12 @@ import pytest
 
 from nemo_gym.token_id_capture import (
     assert_prefix_contiguity,
+    compute_digest,
     per_request,
     prefix_merging,
     project_chain_to_output_items,
     project_main_chain_response,
+    stamp_lineage,
     token_id_capture_dirs_from_config,
     trajectories_for_rollout,
 )
@@ -29,8 +31,8 @@ from nemo_gym.token_id_capture.records import TokenEntry
 from nemo_gym.token_id_capture.store import TokenCaptureStore
 
 
-def _entry(mcid, prompt, gen, lp=None, created_at=0.0):
-    return TokenEntry(
+def _entry(mcid, prompt, gen, parent=None, lp=None, created_at=0.0):
+    e = TokenEntry(
         rollout_id="t0-r0",
         model_call_id=mcid,
         model="m",
@@ -40,6 +42,8 @@ def _entry(mcid, prompt, gen, lp=None, created_at=0.0):
         # Stamped when the call completed. Chain selection orders on it.
         created_at=created_at,
     )
+    stamp_lineage(e, parent)
+    return e
 
 
 # An append-only 3-call rollout: each call's prompt extends the prior prompt+generation
@@ -208,6 +212,19 @@ def test_projection_handles_content_only_leading_item():
     assert "prompt_token_ids" not in resp["output"][0]
     assert resp["usage"] == {"input_tokens": 3, "output_tokens": 2}  # counts from the token-bearing item
     assert_prefix_contiguity(resp)
+
+
+def test_retry_sibling_is_dropped_and_main_chain_is_deterministic():
+    # c2a and c2b are a retry pair (identical prompt, divergent generation). c3 extends c2a.
+    c1 = _entry("c1", [1, 2, 3], [10, 11])
+    c2a = _entry("c2a", [1, 2, 3, 10, 11, 4], [12])
+    c2b = _entry("c2b", [1, 2, 3, 10, 11, 4], [99])
+    c3 = _entry("c3", [1, 2, 3, 10, 11, 4, 12, 5], [13])
+    out = prefix_merging([c1, c2a, c2b, c3])
+    assert "c2b" in out.quarantined  # unextended retry sibling dropped
+    main = next(c for c in out.chains if c.chain_id == "main")
+    assert [link.entry.model_call_id for link in main.links] == ["c1", "c2a", "c3"]
+    assert_prefix_contiguity(project_main_chain_response("t0-r0", out))
 
 
 def test_consumer_reads_store_and_builds(tmp_path):
@@ -440,6 +457,111 @@ def test_the_builder_runs_once_per_rollout(tmp_path, monkeypatch):
 
     assert calls == ["prefix_merging"]
     assert built["metrics"]["n_calls"] == 2
+
+
+def _with_lineage(entry, parent_call_id=None):
+    stamp_lineage(entry, parent_call_id)
+    return entry
+
+
+def test_recorded_parent_link_resolves_a_final_call_retry_exactly():
+    """Two siblings share a prompt and differ only in their generation.
+
+    Prefix matching cannot tell which one the harness kept, because both are
+    equally valid children. A recorded parent link on the next call names the
+    survivor, so the other is provably unused rather than tie-broken.
+    """
+    root = _with_lineage(_entry("root", [1, 2], [3]))
+    kept = _with_lineage(_entry("kept", [1, 2, 3, 4], [5]), parent_call_id="root")
+    dropped = _with_lineage(_entry("dropped", [1, 2, 3, 4], [9]), parent_call_id="root")
+    # The next call continued `kept`, and says so.
+    nxt = _with_lineage(_entry("next", [1, 2, 3, 4, 5, 6], [7]), parent_call_id="kept")
+
+    out = prefix_merging([root, kept, dropped, nxt])
+    main = next(c for c in out.chains if c.chain_id == "main")
+    assert [link.entry.model_call_id for link in main.links] == ["root", "kept", "next"]
+    assert "dropped" in out.quarantined
+    # Resolved, so nothing is flagged for masking.
+    assert out.notes.unresolved_retries == []
+
+
+def test_unresolvable_final_retry_is_flagged_not_silently_tie_broken():
+    """A retry of the LAST call has no successor to name the survivor. Neither
+    inference nor a parent link can resolve it, so it must be reported so the
+    caller can mask the rollout instead of training on a generation the client
+    may never have received."""
+    root = _with_lineage(_entry("root", [1, 2], [3]))
+    a = _with_lineage(_entry("a", [1, 2, 3, 4], [5]), parent_call_id="root")
+    b = _with_lineage(_entry("b", [1, 2, 3, 4], [9]), parent_call_id="root")
+
+    out = prefix_merging([root, a, b])
+    assert sorted(out.notes.unresolved_retries) == ["a", "b"]
+
+
+def test_a_stale_parent_link_fails_verification_and_falls_back():
+    """A rerun that appended onto a previous attempt's records must not merge two
+    attempts. The digest check catches the bad edge; the builder falls back to
+    prefix matching and reports that it did."""
+    root = _with_lineage(_entry("root", [1, 2], [3]))
+    child = _entry("child", [1, 2, 3, 4], [5])
+    stamp_lineage(child, "root")
+    # Corrupt the recorded parent's digest, as a stale record would.
+    root.digest = compute_digest([42, 42, 42])
+
+    out = prefix_merging([root, child])
+    assert out.notes.parent_link_fallbacks == {"parent_digest_mismatch": 1}
+    # Prefix matching still finds the right parent, so the chain is intact.
+    main = next(c for c in out.chains if c.chain_id == "main")
+    assert [link.entry.model_call_id for link in main.links] == ["root", "child"]
+
+
+def test_parent_link_and_prefix_matching_agree_on_a_clean_rollout():
+    """Parity: with and without recorded links, the same rollout must stitch the
+    same way. This is what makes the lineage fields safe to add before anything
+    populates them."""
+    plain = [
+        _entry("c1", [1, 2, 3], [4, 5]),
+        _entry("c2", [1, 2, 3, 4, 5, 6], [7]),
+        _entry("c3", [1, 2, 3, 4, 5, 6, 7, 8], [9, 10]),
+    ]
+    linked = [
+        _with_lineage(_entry("c1", [1, 2, 3], [4, 5])),
+        _with_lineage(_entry("c2", [1, 2, 3, 4, 5, 6], [7]), parent_call_id="c1"),
+        _with_lineage(_entry("c3", [1, 2, 3, 4, 5, 6, 7, 8], [9, 10]), parent_call_id="c2"),
+    ]
+    matched = prefix_merging(plain)
+    recorded = prefix_merging(linked)
+
+    def shape(out):
+        return [([link.entry.model_call_id for link in c.links], c.root_prompt) for c in out.chains]
+
+    assert shape(matched) == shape(recorded)
+
+
+def test_a_recorded_parent_is_verified_not_trusted():
+    """A rerun appends to the same rollout file, so a record can name a parent from a previous
+    attempt whose tokens are an unrelated sequence. Chaining the two would train on a
+    conversation that never happened, so the digest check has to reject it and fall back."""
+    previous_attempt = _entry("old", [1, 2, 3], [4, 5])
+    this_attempt = _entry("new", [90, 91, 92, 93], [94], parent="old")
+
+    out = prefix_merging([previous_attempt, this_attempt])
+
+    assert "parent_digest_mismatch" in out.notes.parent_link_fallbacks
+    for chain in out.chains:
+        assert [link.entry.model_call_id for link in chain.links] != ["old", "new"]
+
+
+def test_a_correct_parent_link_is_used():
+    """The other half: a link that verifies must be honoured, or the recorded lineage buys
+    nothing over matching the parent by token prefix."""
+    parent = _entry("p", [1, 2], [3, 4])
+    child = _entry("c", [1, 2, 3, 4, 5], [6], parent="p")
+
+    out = prefix_merging([parent, child])
+
+    assert out.notes.parent_link_fallbacks == {}
+    assert any([link.entry.model_call_id for link in chain.links] == ["p", "c"] for chain in out.chains)
 
 
 def test_a_chain_that_breaks_is_split_and_reported():